216916607dd709957e7332736fbb0ac5193e2523
Luiz Miguel <luiz.alves.frr@gmail.com>
2026-04-04 20:29:36 -0300

    Vercel Analytics

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { ReactLenis } from 'lenis/react';
 import { LanguageProvider } from './contexts/LanguageContext';
 import './assets/fa-essentials.css';
 import styles from './App.module.css';
+import { Analytics } from "@vercel/analytics/react"
 
 import HeroSection from './components/HeroSection/HeroSection';
 import GallerySection from './components/GallerySection/GallerySection';
@@ -12,6 +13,7 @@ import Footer from './components/Footer/Footer';
 function App() {
   return (
     <LanguageProvider>
+      <Analytics />
       <ReactLenis root options={{ lerp: 0.05, smoothWheel: true }}>
         <div className={styles.appContainer}>
           <HeroSection />
